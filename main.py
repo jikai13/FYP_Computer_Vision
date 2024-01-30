273235d9219a59cffd37bd5c4e94652fbe11816c
jikai-13 <wujikai806@gmail.com>
2024-01-30 16:04:34 +0000

    Implemented Asynchronous Event Loop Adjusted Camera Properties

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import os
 import cv2
 import time
+import asyncio
 
 def control_loop(duration_sec):
 
-    folder_generation(2)
+    #folder_generation(2)
     start_time = time.time()
     end_time = start_time + duration_sec
 
@@ -25,6 +26,8 @@ def control_loop(duration_sec):
 
             take_photo(1)
 
+            take_photo(2)
+
             time.sleep(10)
 
     # GPIO.output('gpio_pin', GPIO.LOW)
@@ -32,6 +35,20 @@ def control_loop(duration_sec):
     # finally:
     #
     #     GPIO.cleanup()
+
+async def async_take_photo(cam_amount):
+    for i in range(cam_amount):
+        #
+
+async def async_control_loop(duration, interval):
+
+    start_time = time.time()
+    end_time = start_time + duration
+
+    while time.time() < end_time:
+        await async_take_photo()
+        await asyncio.sleep(interval) # Delay
+
 
 def folder_generation(cam_amount):
 
@@ -42,6 +59,10 @@ def folder_generation(cam_amount):
 def take_photo(cam_index):
     # Open a connection to the webcam (you can specify the camera index, e.g., 0 for the default camera)
     cap = cv2.VideoCapture(cam_index)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # Sets frame width and height of the video capture
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25) # Adjusts Auto exposure
 
     if not cap.isOpened():
         print("Error: Could not open the camera.")
@@ -56,15 +77,15 @@ def take_photo(cam_index):
         current_time = time.time()
         current_time_struct = time.localtime(current_time)
         timestamp = time.strftime("%Y-%m-%d_%H-%M-%S", current_time_struct)
-        photo_filename = f"D:\FYP\cam{cam_index}_photos/{cam_index}_{timestamp}.jpg"
+        photo_filename = f"D:\FYP\cam{cam_index}_photos/{cam_index}_{timestamp}.tiff"
         cv2.imwrite(photo_filename, frame)
         print("Photo taken!")
 
     # Release the camera and close the window
     cap.release()
 
-def testing():
-    for i in range(10):  # Try indices from 0 to 9
+def cam_index_testing():
+    for i in range(10):
         cap = cv2.VideoCapture(i)
         if cap.isOpened():
             print(f"Camera index {i} opened successfully.")
@@ -72,22 +93,28 @@ def testing():
     else:
         print("Error: Couldn't open any camera.")
 
+def cam_index_testing_singular(cam_index):
+    cap = cv2.VideoCapture(cam_index)
+    if cap.isOpened():
+        print(f"Camera index has opened successfully.")
+
+
+    else:
+        print("Error: Couldn't open any camera.")
+
+
+
 def vid_testing(cam_index):
-    # define a video capture object
+
     vid = cv2.VideoCapture(cam_index)
 
     while (True):
 
-        # Capture the video frame
-        # by frame
         ret, frame = vid.read()
 
         # Display the resulting frame
         cv2.imshow('frame', frame)
 
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -100,9 +127,10 @@ if __name__ == '__main__':
     #control_loop(30)
     #folder_generation(3)
     #take_photo(0)
-    take_photo(1)
+    #take_photo(1)
     #take_photo(2)
     #testing()
-    #vid_testing(1)
+    #testing_specific(1)
+    vid_testing(0)
 
 
